6d60df7fe68750dfa6ade7b4cb7862d6e8c4dce2
andrzej-falkowski <andrzejfalkowski88@gmail.com>
2024-07-02 23:14:54 +0200

    Fixed data fetching

--- src/components/api.jsx
+++ src/components/api.jsx
@@ -5,36 +5,36 @@ const BASE_URL = "https://api.themoviedb.org/3";
 
 export async function getTrendingMovies() {
   const response = await axios.get(
-    `${BASE_URL}/trending/get-trending?api_key${API_KEY}`,
+    `${BASE_URL}/trending/all/day?language=en-US?api_key${API_KEY}`,
   );
-  console.log(response.data);
-  return response.data;
+  console.log(response.data.results);
+  return response.data.results;
 }
 
 export async function searchMovies(query) {
   const response = await axios.get(
-    `${BASE_URL}/search/search-movies?api_key${API_KEY}?${query}`,
+    `${BASE_URL}/search/search-movie?include_adult=false&language=en-US?api_key${API_KEY}?${query}`,
   );
-  return response.data;
+  return response.data.results;
 }
 
 export async function getMovieDetails(id) {
   const response = await axios.get(
-    `${BASE_URL}/movies/get-movie-details?api_key${API_KEY}?${id}`,
+    `${BASE_URL}/movie/${id}?language=en-US?api_key${API_KEY}?${id}`,
   );
   return response.data;
 }
 
 export async function getMovieCredits(id) {
   const response = await axios.get(
-    `${BASE_URL}/movies//movies/get-movie-credits?api_key${API_KEY}?${id}`,
+    `${BASE_URL}/movie/${id}/credits?language=en-US?api_key${API_KEY}?${id}`,
   );
-  return response.data;
+  return response.data.cast;
 }
 
 export async function getMovieReviews(id) {
   const response = await axios.get(
-    `${BASE_URL}/movies/movies/movies/get-movie-reviews?api_key${API_KEY}?${id}`,
+    `${BASE_URL}/movie/${id}/reviews?language=en-US?api_key${API_KEY}?${id}`,
   );
-  return response.data;
+  return response.data.results;
 }
